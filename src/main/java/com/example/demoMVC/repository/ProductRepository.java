85197c9174090f4f830bee9449112140b8b8af6c
Dima Rakhimov <rakhimov9@gmail.com>
2021-05-08 23:25:28 +0500

    up

--- src/main/java/com/example/demoMVC/repository/ProductRepository.java
+++ src/main/java/com/example/demoMVC/repository/ProductRepository.java
@@ -6,16 +6,17 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ProductRepository {
 
-    private final static List<Product> database = new ArrayList<>();
+    private final static List<Product> database = Collections.synchronizedList(new ArrayList<>());
 
     static {
         database.add(new Product("1", "Сникерс", 50));
-        database.add(new Product("1", "Марс", 45));
-        database.add(new Product("1", "Баути", 55));
+        database.add(new Product("2", "Марс", 45));
+        database.add(new Product("3", "Баути", 55));
     }
 
     public void add(Product product) {
@@ -24,6 +25,11 @@ public class ProductRepository {
 
     public List<Product> findAll() {
         return Collections.unmodifiableList(database);
+    }
+
+    public Product getById(String id) {
+        Optional<Product> product= database.stream().filter(p->p.getId().equals(id)).findFirst();
+        return product.get();
     }
 
 }
